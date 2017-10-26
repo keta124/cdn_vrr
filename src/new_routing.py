f68fc6f65bd334e086e64b245dcc0f8ce416b5aa
keta124 <keta124@gmail.com>
2017-10-26 22:38:26 +0700

    Update new_routing.py

--- src/new_routing.py
+++ src/new_routing.py
@@ -19,14 +19,13 @@ class Map_route(object):
         elif "fpt01" in datacenter:
             return'vt02'
     def map_new_route(self,row):
-    	#fields = ('timestamp','network','requesttime','weight','state','customer_isp','datacenter')
-        #Table_dc = namedtuple('Table_dc',fields)
         row['datacenter'] =Map_route().new_route(row['datacenter'])
         return row
-
+'''
 if __name__ == '__main__':
 	y = [{'a':{'a':1,'b':2,'datacenter':'vdc01'}},{'b':{'a':1,'b':2,'datacenter':'vt01'}}]
 	print x[0].values()
 	y = dict(map(lambda x: {x.keys():x.values().Map_route().map_new_route()},y))
 	#y = list(map(lambda x: x,y))
 	print y
+'''
